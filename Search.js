a7754b1b2671ec739b82b8078e81726636d26db5
Aaron Josephs <ajoseph4@binghamton.edu>
2013-05-20 14:53:11 -0400

    added global page

--- Search.js
+++
@@ -1,8 +0,0 @@
-function handleMessage(msgEvent) {
-    var messageName = msgEvent.name;
-    var message = msgEvent.message;
-    if (message === "search") {
-    }
-}
-safari.self.addEventListener("message", handleMessage, false);
-
